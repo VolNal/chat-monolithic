a6d86cc4aa73cbf0b55896c510c21618b37e0d55
Alexandr <sshkajob@gmail.com>
2023-03-26 15:36:17 +0700

    Добавлен проект jwt.

---
+++ src/JwtIdentity/Services/AuthOptionsService.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtIdentity.Services;
+
+public class AuthOptionsService
+{
+    public const string ISSUER = "ProjectJwtIdentity";
+    public const string AUDIENCE = "ProjectJwtIdentity.com";
+    private const string KEY = "0x7e57b0920210d400000";
+
+    public static SymmetricSecurityKey GetSymmetricSecurityKey()
+    {
+        return new(Encoding.UTF8.GetBytes(KEY));
+    }
+}
---
+++ src/JwtIdentity/StartupFilters/IdentityFilter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace JwtIdentity.StartupFilters;
+
+public class IdentityFilter : IStartupFilter
+{
+    public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+    {
+        return app =>
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+            next(app);
+        };
+    }
+}
